f43dee9f078dda64bebef05c203656a4ce353e8d
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-02-25 22:32:03 +0300

    Добавили решение 3 задачи

--- HW_2/task_3.py
+++ HW_2/task_3.py
@@ -1,1 +1,8 @@
 # Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+n = int(input("Введите, чему равно число N: "))
+
+degree_of_two = 1
+while degree_of_two <= n:
+    print(degree_of_two)
+    degree_of_two *= 2
